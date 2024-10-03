88de95ab1875987d73e2bc239eceb41d0e67d59b
niccoratchet <145480429+niccoratchet@users.noreply.github.com>
2024-10-03 12:11:41 +0200

    Added timestamps to photos
    Now photos trasnfered from the camera to the webserver have a timestamp in order to distinguish them.

--- flask-app/app/routes.py
+++ flask-app/app/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request, current_app
 from app.models import Shelf
+from datetime import datetime
 import os
 
 main = Blueprint('main', __name__)      # Create a Blueprint object
@@ -28,17 +29,19 @@ def upload_image():
         return jsonify({"error": "No selected file"}), 400
 
     if file:
-        filename = file.filename
+
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")                           # Get the current timestamp
+
+        filename = f'photo_{timestamp}.jpg'                                            # Create a filename with the timestamp
         file_path = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)         # Path to save the uploaded file
         try:
             file.seek(0, os.SEEK_END)
             file_length = file.tell()
             file.seek(0)                                                                # Reset the file pointer to the beginning
             current_app.logger.info(f"File size: {file_length} bytes")
-
             file.save(file_path)
-            current_app.logger.info(f"File {filename} uploaded successfully to {file_path}")
+            current_app.logger.info(f" File {filename} uploaded successfully to {file_path}")
             return jsonify({"message": f"File uploaded successfully!"}), 200
         except Exception as e:
-            current_app.logger.error(f"Failed to upload file {filename}: {e}")
+            current_app.logger.error(f" Failed to upload file {filename}: {e}")
             return jsonify({"error": "File upload failed"}), 500
